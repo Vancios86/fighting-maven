cdf73a4f54dab5755f384daa5922c3ce98338ceb
Ion Albu <ialbu.webdev@gmail.com>
2022-02-24 16:59:49 +0000

    add global css color values, define container grid termplate

--- interface/src/Components/Main/Main.tsx
+++ interface/src/Components/Main/Main.tsx
@@ -1,19 +1,30 @@
-import "./Main.css"
+import './Main.css';
 
 const Main = () => {
-    return (
-        <div className="main-display">
-            <div className="next-event">
-                <h3>Next event</h3>
-                <div className="next-event-container"></div>
-            </div>
-            <div className="previous-event">
-                <h3>Previous event</h3>
-                <div className="previous-event-container"></div>
-            </div>
-
+  return (
+    <div className='main-display'>
+      <div className='next-event'>
+        <h3>Next event</h3>
+        <div className='next-event-container'>
+          <div className='main-event'></div>
+          <div className='co-main-event'></div>
+          <div className='fight1'></div>
+          <div className='fight2'></div>
+          <div className='fight3'></div>
         </div>
-    )
-}
+      </div>
+      <div className='previous-event'>
+        <h3>Previous event</h3>
+        <div className='previous-event-container'>
+          <div className='main-event'></div>
+          <div className='co-main-event'></div>
+          <div className='fight1'></div>
+          <div className='fight2'></div>
+          <div className='fight3'></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Main;

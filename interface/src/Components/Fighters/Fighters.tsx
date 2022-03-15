cb54311afe0144cc70074f7bec131e9a67430cc9
Ion Albu <ialbu.webdev@gmail.com>
2022-03-15 18:48:19 +0000

    define fighters component, use mock data

--- interface/src/Components/Fighters/Fighters.tsx
+++ interface/src/Components/Fighters/Fighters.tsx
@@ -1,10 +1,27 @@
+import './Fighters.css';
+import data from './Data/fighters.json';
+
 const Fighters: React.FC = () => {
-        return (
-            <div>
-                <h1>fighters from Ukraine</h1>
+  return (
+    <div className='fighters-area'>
+      <div className='fighters-grid'>
+        {data.fighters.map((item, key) => {
+          return (
+            <div key={key} className='fighter-container'>
+              <h2>
+                {item.firstName} {item.lastName}
+              </h2>
+              <h3>"{item.nickname}"</h3>
+              <h3>age: {item.age}</h3>
+              <h3>height: {item.height}</h3>
+              <h3>weight: {item.weight}</h3>
+              <h3>reach: {item.reach}</h3>
             </div>
-        )
-    
-    }
-   
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
 export default Fighters;
